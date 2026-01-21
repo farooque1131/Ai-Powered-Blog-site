59f7482960a709dba7dd54e1f33fd70f9742a84a
Farooque Shaikh <137100902+farooque1131@users.noreply.github.com>
2026-01-21 22:44:35 +0530

    Updated the apps config apps.py

--- blogcore/blog_main/apps.py
+++ blogcore/blog_main/apps.py
@@ -1,27 +1,30 @@
 from django.apps import AppConfig
 from django.contrib.auth.models import User
+from django.db.models.signals import post_migrate
 import os   
 
-User = get_user_model()
+def create_default_superuser(sender, **kwargs):
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+
+    admin_username = os.getenv("ADMIN_USERNAME", "farooque")
+    admin_password = os.getenv("ADMIN_PASSWORD", "Farooque@123")
+    admin_email = os.getenv("ADMIN_EMAIL", "admin@example.com")
+
+    if not User.objects.filter(username=admin_username).exists():
+        User.objects.create_superuser(
+            username=admin_username,
+            email=admin_email,
+            password=admin_password
+        )
+        print("✅ Auto superuser created")
+
+
 class BlogMainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog_main'
-    
+
     def ready(self):
+        post_migrate.connect(create_default_superuser, sender=self)
         import blog_main.signals
-        try:
-            admin_username = os.getenv("ADMIN_USERNAME", "farooque")
-            admin_password = os.getenv("ADMIN_PASSWORD", "Farooque@123")
-            admin_email = os.getenv("ADMIN_EMAIL", "admin@example.com")
-
-            if not User.objects.filter(username=admin_username).exists():
-                User.objects.create_superuser(
-                    username=admin_username,
-                    email=admin_email,
-                    password=admin_password
-                )
-                print("✅ Auto superuser created")
-
-        except Exception as e:
-            print("❌ Superuser creation skipped:", e)
-    
